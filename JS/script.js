6e1d04c3501a7b7937aec94bd954ef6e2efaaa75
AB-Niloy <arnob080100@gmail.com>
2026-02-23 16:25:58 +0600

    added the count function to calculate total , interview & rejected job application in the header section

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-console.log('Assignment 4');
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interviewCount');
+let rejectedCount = document.getElementById('rejectedCount');
+
+const allCards = document.getElementById('all-cards')
+
+function calculateCount() {
+    total.innerText = allCards.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount();
