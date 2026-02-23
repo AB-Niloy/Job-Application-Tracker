c47ccee7a60f61289cf3b5d44b012fc65d216c78
AB-Niloy <arnob080100@gmail.com>
2026-02-23 19:37:02 +0600

    added the toggle function to the all interview & rejected buttons

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,11 @@ let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
 let rejectedCount = document.getElementById('rejectedCount');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+
 const allCards = document.getElementById('all-cards')
 
 function calculateCount() {
@@ -13,3 +18,23 @@ function calculateCount() {
     rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+
+
+function toggleStyle(id) {
+    console.log(id);
+
+    allFilterBtn.classList.add('bg-white', 'text-[#64748B]');       
+    interviewFilterBtn.classList.add('bg-white', 'text-[#64748B]');       
+    rejectedFilterBtn.classList.add('bg-white', 'text-[#64748B]');    
+
+    allFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');       
+    interviewFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');       
+    rejectedFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white'); 
+    
+    const selected = document.getElementById(id);
+
+    selected.classList.remove('bg-white', 'text-[#64748B]');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
+    
+    
+}
